d40f915a4de87756aff000bdec1d36f21ee1dffb
robtrot <32442088+robtrot@users.noreply.github.com>
2017-11-11 10:30:15 -0600

    Add files via upload
    Source file

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './BusinessList.css';
+
+'../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return
+    (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+     </div>
+ )
+  }
+}
+
+export default BusinessList;
